223f4aaaf12156bb106d8b6b917c66d548712c96
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-09 12:14:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,14 @@ cd ~
 wget -O babylon.sh https://node101.io/testnet/babylon.sh
 chmod +x babylon.sh
 ./babylon.sh
+sudo systemctl stop babylond
 
 if [ -f ~/scripts/babylon/config/env ]
  then
+   echo "Config file found."
+ else
   read -p "Wallet name? " WALLET
-  read -p "Moniker? " MONIKER
+  read -p "Moniker name? " MONIKER
   echo "WALLET="$WALLET >> ~/scripts/babylon/config/env
   echo "MONIKER="$MONIKER > ~/scripts/babylon/config/env
 fi
