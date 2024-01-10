d30105ff8c615f94de5cfc291bc4f33e1ec1cf70
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-10 21:53:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -90,5 +90,29 @@ curl -L https://snapshots.kjnodes.com/babylon-testnet/snapshot_latest.tar.lz4 | 
 babylond keys add $WALLET
 babylond create-bls-key $(babylond keys show $WALLET -a)
 
+#create service
+sudo tee /etc/systemd/system/babylon.service > /dev/null << EOF
+[Unit]
+Description=babylon node service
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=$HOME/.babylond"
+Environment="DAEMON_NAME=babylond"
+Environment="UNSAFE_SKIP_BACKUP=true"
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/.babylond/cosmovisor/current/bin"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable babylon.service
+
 echo "Done."
 
