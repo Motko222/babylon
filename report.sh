b5aca237398b52ee51217e0ff6fa237355af2fc4
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-10 09:16:05 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -2,10 +2,13 @@
 
 source ~/scripts/babylon/config/env
 
+awk -F “text” '{print $NF}'
+
 json=$(curl -s localhost:16457/status | jq .result.sync_info)
 
 now=$(date +'%y-%m-%d %H:%M')
 pid=$(pgrep babylond)
+version=$(cat babylon.log | grep "ABCI Handshake App Info" | awk -F "software-version=" '{print $NF}')
 foldersize1=$(du -hs ~/.babylond | awk '{print $1}')
 foldersize2=$(du -hs ~/babylon | awk '{print $1}')
 logsize=$(du -hs ~/logs/babylon.log | awk '{print $1}')
@@ -33,7 +36,7 @@ echo "note='$note'"
 echo "network='$network'"
 echo "type="$type
 echo "folder1=$foldersize1"
-echo "folder2=$foldersize"
+echo "folder2=$foldersize2"
 echo "log=$logsize" 
 echo "id=$MONIKER" 
 echo "wallet=$WALLET"
