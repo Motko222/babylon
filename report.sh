ccaf643bae3e1bb0d19c84d2f74f81be9a2867e3
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-10 09:05:20 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -1,3 +1,40 @@
 #!/bin/bash
 
-curl -s localhost:26657/status | jq .result.sync_info
+source ~/scripts/babylon/config/env
+
+json=$(curl -s localhost:16457/status | jq .result.sync_info)
+
+now=$(date +'%y-%m-%d %H:%M')
+pid=$(pgrep babylond)
+foldersize1=$(du -hs ~/.babylond | awk '{print $1}')
+foldersize2=$(du -hs ~/babylon | awk '{print $1}')
+logsize=$(du -hs ~/logs/babylon.log | awk '{print $1}')
+latestBlock=$(echo $json | jq.latest_block_height)
+catchingUp=$(echo $json | jq.catching_up)
+
+if catchingUp
+ then 
+  status="warning"
+  note="height=$latestBlock"
+ else 
+  status="ok"
+fi
+
+if [ -z $pid ];
+then status="error";
+ note="not running";
+fi
+
+echo "updated='$now'"
+echo "version='$ver'"
+echo "process='$pid'"
+echo "status="$status
+echo "note='$note'"
+echo "network='$network'"
+echo "type="$type
+echo "folder1=$foldersize1"
+echo "folder2=$foldersize"
+echo "log=$logsize" 
+echo "id=$MONIKER" 
+echo "wallet=$WALLET"
+echo "catchingUp=$catchingUp"
