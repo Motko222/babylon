a70779491fd18acf638b5267534a793b9b81bfee
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-11 09:13:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -98,12 +98,12 @@ Description=babylon node service
 After=network-online.target
 
 [Service]
-User=$USER
+User=root
 ExecStart=$(which cosmovisor) run start
 Restart=on-failure
 RestartSec=10
 LimitNOFILE=65535
-Environment="DAEMON_HOME=$HOME/.babylond"
+Environment="DAEMON_HOME=/root/.babylond"
 Environment="DAEMON_NAME=babylond"
 Environment="UNSAFE_SKIP_BACKUP=true"
 Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/.babylond/cosmovisor/current/bin"
@@ -115,5 +115,5 @@ EOF
 sudo systemctl daemon-reload
 sudo systemctl enable babylon.service
 
-echo "Done."
+echo "Installation done, service is not started. Please run it with start-service.sh."
 
