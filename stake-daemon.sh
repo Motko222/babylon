73c8351421329b495b44fb35d3dd45d1cb6ef585
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-11 13:34:17 +0100

    Update stake-daemon.sh

--- stake-daemon.sh
+++ stake-daemon.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+ exeu#!/bin/bash
 
 while true
 do
  echo $(date +'%y-%m-%d %H:%M') >> ~/logs/bbn-stake.txt
  bash ~/scripts/babylon/stake.sh >> ~/logs/bbn-stake.txt
  delay=$(( $RANDOM % 3600 + 3600 ))s
- echo "Waiting $delay" >> ~/logs/bbn-stake.txt
+ echo "Waiting for next execution $delay" >> ~/logs/bbn-stake.txt
  sleep $delay
 done
