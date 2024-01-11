61d484bd891a25c03efa961c3cefa1038bf4d27f
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-11 22:49:19 +0100

    Update summary.sh

--- summary.sh
+++ summary.sh
@@ -3,7 +3,7 @@
 source ~/scripts/babylon/config/env
 
 echo   "---- SUMMARY --------------------------------------------------------------------------"
-printf "%-12s %9s %9s\n" Id Balance Stake
+printf "%-12s %9s %9s %9s\n" Id Balance Delegated Reward
 echo   "---------------------------------------------------------------------------------------"
 
 babylond keys list | grep -E 'name|address' | sed 's/- address: //g' | sed 's/  name: //g' | paste - - | grep -v master >~/scripts/babylon/config/keys
@@ -14,10 +14,11 @@ do
    wallet=$(echo $line | awk '{print $1}')
    balance=$(babylond query bank balances $wallet | grep amount | awk '{print $3}' | sed 's/"//g' | awk '{print $1/1000000}')
    valoper=$(babylond keys show $WALLET --bech val | grep valoper | awk '{print $3}')
+   rewards=$(babylond query distribution rewards $wallet $valoper | cut -d . -f 1 | | awk '{print $1/1000000}')
    
    stake=$(babylond query staking delegation $wallet $valoper 2>/dev/null | grep amount | \
       awk '{print $2}' | sed 's/"//g' | awk '{print $1/1000000}' )
 
-   printf "%-12s %9s %9s\n" \
-      $id $balance $stake
+   printf "%-12s %9s %9s %9s\n" \
+      $id $balance $stake $rewards
 done
