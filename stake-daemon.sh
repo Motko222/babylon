16f58a88b9567ac3a82452061b30db5f8c366091
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-11 23:26:42 +0100

    Update stake-daemon.sh

--- stake-daemon.sh
+++ stake-daemon.sh
@@ -1,10 +1,10 @@
- exeu#!/bin/bash
+#!/bin/bash
 
 while true
 do
- echo $(date +'%y-%m-%d %H:%M') >> ~/logs/bbn-stake.txt
+ echo "Daemon started: "$(date +'%y-%m-%d %H:%M') >> ~/logs/bbn-stake.txt
  bash ~/scripts/babylon/stake.sh >> ~/logs/bbn-stake.txt
- delay=$(( $RANDOM % 3600 + 3600 ))s
- echo "Waiting for next execution $delay" >> ~/logs/bbn-stake.txt
- sleep $delay
+ #delay=$(( $RANDOM % 3600 + 3600 ))s
+ #echo "Waiting for next execution $delay" >> ~/logs/bbn-stake.txt
+ #sleep $delay
 done
