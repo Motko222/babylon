4fd4714a96080db6f061878d70267ff6e4569457
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-11 22:55:26 +0100

    Update summary.sh

--- summary.sh
+++ summary.sh
@@ -14,10 +14,11 @@ do
    wallet=$(echo $line | awk '{print $1}')
    balance=$(babylond query bank balances $wallet | grep amount | awk '{print $3}' | sed 's/"//g' | awk '{print $1/1000000}')
    valoper=$(babylond keys show $WALLET --bech val | grep valoper | awk '{print $3}')
-   rewards=$(babylond query distribution rewards $wallet $valoper 2>/dev/null | awk '{print $1/1000000}')
+   rewards=$(babylond query distribution rewards $wallet $valoper 2>/dev/null \
+     | grep amount | awk '{print $3}' | sed 's/"//g' | awk '{print $1/1000000}')
    
-   stake=$(babylond query staking delegation $wallet $valoper 2>/dev/null | grep amount | \
-      awk '{print $2}' | sed 's/"//g' | awk '{print $1/1000000}' )
+   stake=$(babylond query staking delegation $wallet $valoper 2>/dev/null \
+     | grep amount | awk '{print $2}' | sed 's/"//g' | awk '{print $1/1000000}' )
 
    printf "%-12s %9s %9s %9s\n" \
       $id $balance $stake $rewards
