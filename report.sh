a04824d37bd919b4b63557a433a5dacce98a87b2
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-12 11:15:45 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -9,7 +9,7 @@ pid=$(pgrep babylond)
 ver=$(cat ~/logs/babylon.log | grep "ABCI Handshake App Info" | awk -F "software-version=" '{print $NF}')
 foldersize1=$(du -hs ~/.babylond | awk '{print $1}')
 foldersize2=$(du -hs ~/babylon | awk '{print $1}')
-logsize=$(du -hs ~/logs/babylon.log | awk '{print $1}')
+#logsize=$(du -hs ~/logs/babylon.log | awk '{print $1}')
 latestBlock=$(echo $json | jq .latest_block_height | sed 's/"//g' )
 catchingUp=$(echo $json | jq .catching_up)
 votingPower=$(babylond status 2>&1 | jq .ValidatorInfo.VotingPower | sed 's/"//g')
@@ -41,7 +41,7 @@ echo "network='$network'"
 echo "type="$type
 echo "folder1=$foldersize1"
 echo "folder2=$foldersize2"
-echo "log=$logsize" 
+#echo "log=$logsize" 
 echo "id=$MONIKER" 
 echo "wallet=$WALLET"
 echo "catchingUp=$catchingUp"
