ecea33df3d78dee3c0979852055240242b774f5a
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-12 09:13:54 +0100

    Update delegate.sh

--- delegate.sh
+++ delegate.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 source ~/scripts/babylon/config/env
 
-babylond keys list | grep -E 'name|address' | sed 's/- address: //g' | sed 's/  name: //g' | paste - - | grep -v master >~/logs/bbn-keys
-valoper=$(babylond keys show $WALLET --bech val | grep valoper | awk '{print $3}')
+if [ -z $1 ]
+then
+ read -p "From wallet ? " wallet
+else
+ fromWallet=$1
+fi
 
-echo WALLETS
+if [ -z $2 ]
+then
+ read -p "To valoper ? " valoper
+else
+ toValoper=$2
+fi
 
-cat ~/logs/bbn-keys | while read line
-do
-   id=$(echo $line | awk '{print $2}')
-   wallet=$(echo $line | awk '{print $1}')
-   balance=$(babylond query bank balances $wallet | grep amount | awk '{print $3}' | sed 's/"//g' )
+if [ -z $3 ]
+then
+ read -p "Amount  ? " amount
+else
+ amount=$3
+fi
 
-   printf "%-12s %-45s %12s/n" $id $wallet $balance
+amount=$(( $amount * 1000000 ))ubbn
 
-done
-
-echo
-read -p "Wallet ? " fromWallet
-read -p "Valoper (blank=current)? " toValoper
-if [ -z $toValoper ]; then toValoper=$valoper; fi
-read -p "Amount (ubbn)? " toStake
-
-babylond tx epoching delegate $toValoper $toStake --from $fromWallet \
- --chain-id bbn-test-2 --gas-prices 0.1ubbn --gas-adjustment 1.5 --gas auto -y 
+babylond tx epoching delegate $valoper $amount --from $wallet \
+ --chain-id bbn-test-2 --gas-prices 0.1ubbn --gas-adjustment 1.5 --gas auto -y
