0cc0aeca5a2e2deaa9126391142f42f42651966d
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-14 18:20:57 +0100

    Update send.sh

--- send.sh
+++ send.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 
+source ~/scripts/babylon/config/env
+
+if [ -z $1 ]
+then
+ read -p "From wallet ? " from
+else
+ wallet=$1
+fi
+
+if [ -z $2 ]
+then
+ read -p "To wallet ? " to
+else
+ valoper=$2
+fi
+
+if [ -z $3 ]
+then
+ read -p "Amount ? " amount
+else
+ amount=$3
+fi
+
+amount=$(( $amount * 1000000 ))ubbn
+
+babylond tx bank send $from $to $amount \
+   --gas-prices 0.1ubbn --gas-adjustment 1.5 --gas auto -y
