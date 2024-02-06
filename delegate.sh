af44ca92d74888830057a7b08cbb6e4168e40fac
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-06 21:58:19 +0100

    Update delegate.sh

--- delegate.sh
+++ delegate.sh
@@ -10,7 +10,7 @@ else
 fi
 
 wallet=$(babylond keys show $key --output json | jq -r .address)
-balance=$(babylond query bank balances $wallet -o json | jq -r .amount | awk '{print $1/1000000}')"bbn"
+balance=$(babylond query bank balances $wallet -o json | jq -r .amount | awk '{print $1/1000000}')
 echo "Balance: $balance bbn"
 
 if [ -z $2 ]
