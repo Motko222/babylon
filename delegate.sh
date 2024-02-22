2ef242bdc9908b4efd28c020e571650601eae0a2
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-22 14:00:01 +0100

    Update delegate.sh

--- delegate.sh
+++ delegate.sh
@@ -9,13 +9,13 @@ else
  key=$1
 fi
 
-wallet=$(echo $PWD | $BINARY keys show $key --output json | jq -r .address)
+wallet=$(echo $PSWD | $BINARY keys show $key --output json | jq -r .address)
 balance=$(babylond query bank balances $wallet -o json | jq -r .balances[].amount | awk '{print $1/1000000}')
 echo "Balance: $balance bbn"
 
 if [ -z $2 ]
 then
- def_valoper=$(echo $PWD | $BINARY keys show $key -a --bech val)
+ def_valoper=$(echo $PSWD | $BINARY keys show $key -a --bech val)
  read -p "To valoper (default $def_valoper) ? " valoper
  if [ -z $valoper ]; then valoper=$def_valoper; fi
 else
