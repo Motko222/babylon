1cae29a1174a54cf1213899c1fe12098efc7819e
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-22 10:33:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ sudo apt-get install jq
 
 # INSTALL GO
 sudo rm -rf /usr/local/go
-curl -Ls https://go.dev/dl/go1.20.12.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+curl -Ls https://go.dev/dl/go1.22.0.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
 eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
 eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
 go version
@@ -59,15 +59,15 @@ babylond config node tcp://localhost:16457
 babylond init $MONIKER --chain-id $NETWORK
 
 # Download genesis and addrbook
-curl -Ls https://snapshots.kjnodes.com/babylon-testnet/genesis.json > $HOME/.babylond/config/genesis.json
-curl -Ls https://snapshots.kjnodes.com/babylon-testnet/addrbook.json > $HOME/.babylond/config/addrbook.json
+# curl -Ls https://snapshots.kjnodes.com/babylon-testnet/genesis.json > $HOME/.babylond/config/genesis.json
+# curl -Ls https://snapshots.kjnodes.com/babylon-testnet/addrbook.json > $HOME/.babylond/config/addrbook.json
 
 # Add seeds
-#sed -i -e "s|^seeds *=.*|seeds = \"3f472746f46493309650e5a033076689996c8881@babylon-testnet.rpc.kjnodes.com:16459\"|" $HOME/.babylond/config/config.toml
+# sed -i -e "s|^seeds *=.*|seeds = \"3f472746f46493309650e5a033076689996c8881@babylon-testnet.rpc.kjnodes.com:16459\"|" $HOME/.babylond/config/config.toml
 
 # Add persistent_peers
-peers="3287ddbe454cfb5a162799b468c1405b7370c541@95.217.199.12:26601,d5519e378247dfb61dfe90652d1fe3e2b3005a5b@65.109.68.190:16456,cf34c3413b515e0043a279e6085b95e153c50871@168.119.10.134:26656,6f55a3138d18e8c0eaa66e347794428db698bf2a@148.251.176.236:2000,895522d0a1d174b7810f0c4904261f6f014fb193@161.97.123.214:16456,b6300c3bf3a9fa9492c1c7e970265e9dad8d2929@95.216.244.226:16456,0de44b3d4380004838d38797a1aee10392b68420@3.142.201.103:26656,2d7f98f9080467d1d084c6ecfeffc2275de3c3e0@38.242.239.210:16456,9eea44710677f6bca53a30cd5070fb6820b38a77@161.97.114.31:16456,d98fd2665253fc469d861752277ae4732029ca4a@178.18.254.152:16456,aa0756ebb34536d4cee93683ebe135e9704e907d@208.93.66.43:16456,97619188659de26559231d93d7e27be6e58133b7@185.215.166.212:16456,357aedc0f46a7f69f6112099dd135e425c745add@31.220.74.5:16456,f3e39d6d0f3baccc9a46dd9558c171af2070baeb@78.196.234.246:26656,ac4ce172df2d437f63152dd7a8c026a5e283aa19@65.109.175.84:16456,1dd53ef6e8b5290cc766236d56ebe308ff27356f@185.230.138.85:16456,77166640af158920a87186725ecea40b4310e14d@5.189.173.161:16456,bbf8ef70a32c3248a30ab10b2bff399e73c6e03c@65.21.198.100:21756,c78bfc22b8fe6a913520228b75078554758e4b41@185.202.238.243:16456,b32c6ac95268080ec1f336b57a7cab07e7b6f407@38.242.243.17:16456,f2037faa4fab89441b91a87fa81b1fcabfaa77ac@161.97.100.158:16456,0a0d7d245ea67cfeec7d000085260fbe695544f4@207.180.251.220:11656"
-sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$peers\"|" $HOME/.babylond/config/config.toml
+#peers="3287ddbe454cfb5a162799b468c1405b7370c541@95.217.199.12:26601,d5519e378247dfb61dfe90652d1fe3e2b3005a5b@65.109.68.190:16456,cf34c3413b515e0043a279e6085b95e153c50871@168.119.10.134:26656,6f55a3138d18e8c0eaa66e347794428db698bf2a@148.251.176.236:2000,895522d0a1d174b7810f0c4904261f6f014fb193@161.97.123.214:16456,b6300c3bf3a9fa9492c1c7e970265e9dad8d2929@95.216.244.226:16456,0de44b3d4380004838d38797a1aee10392b68420@3.142.201.103:26656,2d7f98f9080467d1d084c6ecfeffc2275de3c3e0@38.242.239.210:16456,9eea44710677f6bca53a30cd5070fb6820b38a77@161.97.114.31:16456,d98fd2665253fc469d861752277ae4732029ca4a@178.18.254.152:16456,aa0756ebb34536d4cee93683ebe135e9704e907d@208.93.66.43:16456,97619188659de26559231d93d7e27be6e58133b7@185.215.166.212:16456,357aedc0f46a7f69f6112099dd135e425c745add@31.220.74.5:16456,f3e39d6d0f3baccc9a46dd9558c171af2070baeb@78.196.234.246:26656,ac4ce172df2d437f63152dd7a8c026a5e283aa19@65.109.175.84:16456,1dd53ef6e8b5290cc766236d56ebe308ff27356f@185.230.138.85:16456,77166640af158920a87186725ecea40b4310e14d@5.189.173.161:16456,bbf8ef70a32c3248a30ab10b2bff399e73c6e03c@65.21.198.100:21756,c78bfc22b8fe6a913520228b75078554758e4b41@185.202.238.243:16456,b32c6ac95268080ec1f336b57a7cab07e7b6f407@38.242.243.17:16456,f2037faa4fab89441b91a87fa81b1fcabfaa77ac@161.97.100.158:16456,0a0d7d245ea67cfeec7d000085260fbe695544f4@207.180.251.220:11656"
+#sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$peers\"|" $HOME/.babylond/config/config.toml
 
 # Set minimum gas price
 sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.00001ubbn\"|" $HOME/.babylond/config/app.toml
@@ -85,8 +85,8 @@ sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.
 sed -i -e "s%^address = \"tcp://localhost:1317\"%address = \"tcp://0.0.0.0:16417\"%; s%^address = \":8080\"%address = \":16480\"%; s%^address = \"localhost:9090\"%address = \"0.0.0.0:16490\"%; s%^address = \"localhost:9091\"%address = \"0.0.0.0:16491\"%; s%:8545%:16445%; s%:8546%:16446%; s%:6065%:16465%" $HOME/.babylond/config/app.toml
 
 # Download latest chain snapshot
-curl -L https://snapshots.kjnodes.com/babylon-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.babylond
-[[ -f $HOME/.babylond/data/upgrade-info.json ]] && cp $HOME/.babylond/data/upgrade-info.json $HOME/.babylond/cosmovisor/genesis/upgrade-info.json
+#curl -L https://snapshots.kjnodes.com/babylon-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.babylond
+#[[ -f $HOME/.babylond/data/upgrade-info.json ]] && cp $HOME/.babylond/data/upgrade-info.json $HOME/.babylond/cosmovisor/genesis/upgrade-info.json
 
 babylond keys add $WALLET --recover
 babylond create-bls-key $(babylond keys show $WALLET -a)
