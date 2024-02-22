c9495b5ba8103a12b79c1fc63217993a741aa759
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-22 12:21:36 +0100

    Update summary.sh

--- summary.sh
+++ summary.sh
@@ -6,20 +6,20 @@ echo   "---- SUMMARY -----------------------------------------------------------
 printf "%-12s %9s %9s %9s\n" Id Balance Delegated Reward
 echo   "---------------------------------------------------------------------------------------"
 
-babylond keys list | grep -E 'name|address' | sed 's/- address: //g' | sed 's/  name: //g' | paste - - | grep -v master >~/scripts/babylon/config/keys
+echo $PSWD | babylond keys list | grep -E 'name' | sed 's/  name: //g' >~/scripts/babylon/config/keys
 
 cat ~/scripts/babylon/config/keys | while read line
 do
-   id=$(echo $line | awk '{print $2}')
-   wallet=$(echo $line | awk '{print $1}')
+   key=$(echo $line | awk '{print $1}')
+   wallet=$(echo $PSWD | babylond keys show $key -a) 
    balance=$(babylond query bank balances $wallet | grep amount | awk '{print $3}' | sed 's/"//g' | awk '{print $1/1000000}')
-   valoper=$(babylond keys show $WALLET --bech val | grep valoper | awk '{print $3}')
+   valoper=$(echo $PSWD | babylond keys show $KEY --bech val | grep valoper | awk '{print $3}')
    rewards=$(babylond query distribution rewards $wallet $valoper 2>/dev/null \
      | grep amount | awk '{print $3}' | sed 's/"//g' | awk '{print $1/1000000}')
-   
+
    stake=$(babylond query staking delegation $wallet $valoper 2>/dev/null \
      | grep amount | awk '{print $2}' | sed 's/"//g' | awk '{print $1/1000000}' )
 
    printf "%-12s %9s %9s %9s\n" \
-      $id $balance $stake $rewards
+      $key $balance $stake $rewards
 done
