7b9dd0b2658e26f5bf1cfc7688661bebed3bf88b
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-23 11:15:22 +0100

    Update withdraw-commission.sh

--- withdraw-commission.sh
+++ withdraw-commission.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+FOLDER=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+source ~/scripts/$FOLDER/config/env
+
+valoper=$(echo $PSWD | $BINARY keys show $KEY -a --bech val)
+
+echo $PSWD | babylond tx distribution withdraw-rewards $valoper \
+  --chain-id $NETWORK --from $KEY --commission --gas $GAS --gas-adjustment $GAS_ADJ --gas-prices $GAS_PRICE -y
+
