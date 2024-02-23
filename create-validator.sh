053ee87bda2c1b7f5aa8c3e3471ef4a15b3e4b49
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-23 22:35:29 +0100

    Delete create-validator.sh

--- create-validator.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source ~/scripts/babylon/config/env
-
-echo $PSWD | babylond tx checkpointing create-validator /root/scripts/babylon/validator.json \
---from $KEY --gas-adjustment $GAS_ADJ --gas $GAS --fees 10ubbn -y
