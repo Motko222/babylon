c8fc287f16cd4d8c0ffc988be4b299859410591a
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-23 10:36:39 +0100

    Update send.sh

--- send.sh
+++ send.sh
@@ -4,10 +4,14 @@ source ~/scripts/babylon/config/env
 
 if [ -z $1 ]
 then
- read -p "From wallet ? " from
+ read -p "From key ? " from
 else
  from=$1
 fi
+
+wallet=$(echo $PSWD | $BINARY keys show $key --output json | jq -r .address)
+balance=$(babylond query bank balances $wallet -o json | jq -r .balances[].amount | awk '{print $1/1000000}')
+echo "Balance: $balance bbn"
 
 if [ -z $2 ]
 then
@@ -18,7 +22,7 @@ fi
 
 if [ -z $3 ]
 then
- read -p "Amount ? " amount
+ read -p "Amount (bbn) ? " amount
 else
  amount=$3
 fi
