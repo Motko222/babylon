49f7e09207b594201a147b1698e4f8bb5fcfd46c
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-03-01 11:00:45 +0100

    Update unjail.sh

--- unjail.sh
+++ unjail.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ~/scripts/babylon/config/env
 echo $PSWD | $BINARY tx slashing unjail --from $KEY \
-  --chain-id $NETWORK --gas-prices $GAS_PRICE --gas-adjustment $GA_ADJ --gas $GAS -y
+  --chain-id $NETWORK --gas-prices $GAS_PRICE --gas-adjustment $GAS_ADJ --gas $GAS -y
